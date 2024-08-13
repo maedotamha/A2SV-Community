001cafbfde9ef319a7655970208e636da85be154
Maedot <124568412+maedotamha@users.noreply.github.com>
2024-08-13 12:50:22 +0300

    Update List comprehension.py

--- List comprehension.py
+++ List comprehension.py
@@ -3,12 +3,5 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    output = [];
-    abc = [];
-    for X in range(x+1):
-        for Y in range(y+1):
-            for Z in range(z+1):
-                if X+Y+Z != n:
-                    abc = [X,Y,Z];
-                    output.append(abc);
-print(output);    
+    coordinates = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if(i+j+k) != n]
+    print (coordinates)
